d08e0cd6143a1ae2d9ec03f73fc4f2da0b1984b9
Simran Malik <smalik12@charlotte.edu>
2025-08-21 13:32:08 -0400

    Add files via upload

---
+++ sketch_aug3a.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN 9
+#define SS_PIN 10  // NSS = SDA (Slave Select)
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Hold your fob near the reader...");
+}
+
+void loop() {
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  
+  Serial.print("UID: ");
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+  mfrc522.PICC_HaltA();
+}
+
